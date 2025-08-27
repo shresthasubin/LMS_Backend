312bf2cdd25ef43e4dbd98684249eb7de8f03232
shresthasubin <shresthasubin938@gmail.com>
2025-08-27 12:07:33 +0545

    backend

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use('/uploads', express.static('uploads'))
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
 
