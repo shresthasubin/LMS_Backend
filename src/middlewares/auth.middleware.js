050afeee59117de340f1b545fa47706b160bacad
shresthasubin <shresthasubin938@gmail.com>
2025-08-27 20:00:41 +0545

    backend

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken'
 
 const VerifyJWT = async (req, res, next) => {
     try {
-        const token = req.header('Authorization')?.replace('Bearer ', '') || req.cookies?.token
+        const token = req.cookies?.token || req.header('Authorization')?.replace('Bearer ', '') 
         if (!token) {
             return res.status(401).json({
                 success: false,
